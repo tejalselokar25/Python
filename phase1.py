a3e1bfa24ba4b5f35cf2c34d20690f58f4846b11
tejalselokar25 <tejals.ml25@sbjit.edu.in>
2026-08-20 17:12:48 +0530

    Add files via upload

---
+++ phase1.py
@@ -0,0 +1,25 @@
+tree = ["*", ["+", 10, 5], 2]
+
+left = tree[1]
+right = tree[2]
+
+if left[0] == "+":
+    left_result = left[1] + left[2]
+elif left[0] == "-":
+    left_result = left[1] - left[2]
+elif left[0] == "*":
+    left_result = left[1] * left[2]
+elif left[0] == "/":
+    left_result = left[1] / left[2]
+
+if tree[0] == "+":
+    result = left_result + right
+elif tree[0] == "-":
+    result = left_result - right
+elif tree[0] == "*":
+    result = left_result * right
+elif tree[0] == "/":
+    result = left_result / right
+
+print("Expression: (10 + 5) * 2")
+print("Result:", result)
