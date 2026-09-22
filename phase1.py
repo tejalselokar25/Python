ad701f468d165d5e0caad1368bd96b024471f655
Tejal <tejals.ml25@sbjit.edu.in>
2026-09-22 10:48:28 +0530

    Experiential Learning Phase-2

--- phase1.py
+++ phase1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 tree = ["*", ["+", 10, 5], 2]
 
 left = tree[1]
@@ -22,4 +23,30 @@ elif tree[0] == "/":
     result = left_result / right
 
 print("Expression: (10 + 5) * 2")
+=======
+tree = ["*", ["+", 10, 5], 2]
+
+left = tree[1]
+right = tree[2]
+
+if left[0] == "+":
+    left_result = left[1] + left[2]
+elif left[0] == "-":
+    left_result = left[1] - left[2]
+elif left[0] == "*":
+    left_result = left[1] * left[2]
+elif left[0] == "/":
+    left_result = left[1] / left[2]
+
+if tree[0] == "+":
+    result = left_result + right
+elif tree[0] == "-":
+    result = left_result - right
+elif tree[0] == "*":
+    result = left_result * right
+elif tree[0] == "/":
+    result = left_result / right
+
+print("Expression: (10 + 5) * 2")
+>>>>>>> e7216e6 (Experiential Learning Phase-2)
 print("Result:", result)
